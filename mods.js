7184588948a28b916d5af77de754766d37f1bddc
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-10-03 12:56:05 -0700

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'Trimps-QWUI';
+script.src = 'https://stellar-demesne.github.io/Trimps-QWUI/qwUI.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
